0864e8f237cefecee3497932a540adfcc768599b
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-08-30 08:40:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,9 +2,9 @@ from os import getenv as genv
 from pyrogram import Client
 
 
-API_ID = genv("API_ID", "")
+API_ID = genv("API_ID", "7041911")
 
-API_HASH = genv("API_HASH", "")
+API_HASH = genv("API_HASH", "abab2561c71e3004a55d4ff9763d5383")
 
 BOT_TOKEN = genv("BOT_TOKEN", "")
 
