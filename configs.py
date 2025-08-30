8f7c687931d762af339a52f603965675116e0f32
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-08-30 10:03:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,5 +8,5 @@ API_HASH = genv("API_HASH", "abab2561c71e3004a55d4ff9763d5383")
 
 BOT_TOKEN = genv("BOT_TOKEN", "")
 
-WORKER_URL = "https://adda.botzs.workers.dev/?url="
+WORKER_URL = "https://adda.botzs.workers.dev"
 
