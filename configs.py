a5a6f6c5840719b608b678621ffb02f7cd89cea8
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-09-01 11:18:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,24 @@
 from os import getenv as genv
-from pyrogram import Client
+from pymongo import MongoClient
 
-
+# Telegram API credentials
 API_ID = genv("API_ID", "7041911")
-
 API_HASH = genv("API_HASH", "abab2561c71e3004a55d4ff9763d5383")
-
 BOT_TOKEN = genv("BOT_TOKEN", "")
 
+# Worker (if needed)
 WORKER_URL = "https://adda.botzs.workers.dev"
 
+# Owner ID
+OWNER_ID = int(genv("OWNER_ID", "6390511215"))
+
+# MongoDB Atlas URL
+MONGO_URL = genv(
+    "MONGO_URL",
+    "mongodb+srv://scrape:scrape@cluster0.frrzcbo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+
+# MongoDB Setup
+mongo_client = MongoClient(MONGO_URL)
+db = mongo_client["auth_db"]
+groups_col = db["authorized_groups"]
