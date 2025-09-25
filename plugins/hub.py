bcac4ac00e8f48500d7d4178cbe45691759e9932
fakeusername807 <f6235777@gmail.com>
2025-09-25 18:51:25 +0530

    Update hub.py

--- plugins/hub.py
+++ plugins/hub.py
@@ -71,7 +71,7 @@ async def hubcloud_handler(client: Client, message: Message):
 
         # ✅ Requested By (only once, after loop)
         if message.from_user:
-            text += f"<b>Requested By :-</b> {message.from_user.mention}\n<b>(#ID_{message.from_user.id})</b>\n\n"
+            text += f"<b>Requested By :-</b> <b>{message.from_user.mention}</b>\n<b>(#ID_{message.from_user.id})</b>\n\n"
 
         # ✅ Add button
         await wait_msg.edit_text(
