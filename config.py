44a8817c871429acb5402048fb33f4d62f46cd5f
fakeusername807 <f6235777@gmail.com>
2025-09-25 11:02:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from os import getenv as genv
 
 # Telegram API credentials
-API_ID = int(genv("API_ID", "7041911"))
-API_HASH = genv("API_HASH", "abab2561c71e3004a55d4ff9763d5383")
-BOT_TOKEN = genv("BOT_TOKEN", "")
+API_ID = int(genv("API_ID", "26334970"))
+API_HASH = genv("API_HASH", "e7d1141cca9fbe1ab45804163b5080c8")
+BOT_TOKEN = genv("BOT_TOKEN", "7928207862:AAFUk521pf1mHSGUxNf7WOxSm9NSJAR6w98")
 
 # Worker URL
 WORKER_URL = "https://adda.botzs.workers.dev"
 
 # Authorized IDs (comma-separated)
-AUTH = genv("AUTH", "-1002311378229")
+AUTH = genv("AUTH", "-1002645306586")
 
 # Owner ID
-OWNER_ID = int(genv("OWNER_ID", "6390511215"))
+OWNER_ID = int(genv("OWNER_ID", "7965786027"))
